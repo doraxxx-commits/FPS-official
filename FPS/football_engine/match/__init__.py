447749dd9151b7d580a4af6a4304fd0fac2fef87
doraxxx-commits <dorax.xx@o2.pl>
2026-08-22 19:23:42 +0200

    Delete FPS directory

--- FPS/football_engine/match/__init__.py
+++
@@ -1,3 +0,0 @@
-from football_engine.match.simulation import MatchResult, simulate_match
-
-__all__ = ["MatchResult", "simulate_match"]
--- FPS/server.py
+++
@@ -1,5 +0,0 @@
-import os
-from backend.app import app
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
